87459d7b25c3e73962809f72ca70279547a7db82
iaguirre19 <iran.aguirre20@gmail.com>
2023-06-05 20:00:40 -0600

    light version finished

--- assets/js/chnageTheme.js
+++ assets/js/chnageTheme.js
@@ -1,22 +1,40 @@
-const themeButton = document.getElementById('theme-button')
-const lightTheme = 'light-theme'
+const themeButton = document.getElementById("theme-button");
+const lightMode = "light-theme";
 const iconTheme = "bx-moon";
+const sunIcon = "bx-sun";
 
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
-// const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+// We obtain the current theme that the interface has by validating the light-theme class
+const getCurrentTheme = () =>
+    document.body.classList.contains(lightMode) ? "light" : "dark";
+const getCurrentIcon = () =>
+    themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
 
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light theme
+    document.body.classList[selectedTheme === "light" ? "add" : "remove"](
+    lightMode
+  );
+    themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
 
-// const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
-// console.log(getCurrentIcon)
-
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(lightTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    // localStorage.setItem('selected-theme', getCurrentTheme())
-    // localStorage.setItem('selected-icon', getCurrentIcon())
-})
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the light theme / icon theme
+    document.body.classList.toggle(lightMode);
+    if (themeButton.classList.contains(iconTheme)) {
+        themeButton.classList.replace(iconTheme, sunIcon);
+    } else {
+        themeButton.classList.replace(sunIcon, iconTheme);
+    }
+  // We save the theme and the current icon that the user chose
+    localStorage.setItem("selected-theme", getCurrentTheme());
+    localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
