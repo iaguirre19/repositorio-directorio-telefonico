b4e4f8cb7d4f48032940c5de6be7b199af71177b
iaguirre19 <iran.aguirre20@gmail.com>
2023-06-29 10:03:38 -0600

    vite modules cretaed

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,8 +1,8 @@
 const inputsForms = document.querySelectorAll(".login-input");
 const showPasswordIcon = document.querySelector("#showPassword");
 const passwordInput = document.querySelector("#passwordLogin");
-
-
+const loginInput = document.querySelector("#userLogin");
+const btnLogin = document.querySelector("#btnLogin");
 
 activeInputsOnClick();
 
@@ -18,24 +18,31 @@ function activeInputsOnClick() {
       } else {
         input.parentElement.classList.remove("active-error");
         input.parentElement.classList.add("active");
-
       }
     });
   });
+}
+
+const getLoginValues = (userName, password) => {
+  const userNameValue = userName.value;
+  const passwordValue = password.value;
+  console.log(userNameValue);
+  console.log(passwordValue);
 };
 
-showPasswordIcon.addEventListener('click', function() {
-  if (passwordInput.type === 'password') {
-    passwordInput.type = 'text';
-    showPasswordIcon.classList.remove('bx-show');
-    showPasswordIcon.classList.add('bx-hide');
+showPasswordIcon.addEventListener("click", (e) => {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    showPasswordIcon.classList.remove("bx-show");
+    showPasswordIcon.classList.add("bx-hide");
   } else {
-    passwordInput.type = 'password';
-    showPasswordIcon.classList.remove('bx-hide');
-    showPasswordIcon.classList.add('bx-show');
+    passwordInput.type = "password";
+    showPasswordIcon.classList.remove("bx-hide");
+    showPasswordIcon.classList.add("bx-show");
   }
 });
 
-
-
-
+btnLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  getLoginValues(loginInput, passwordInput);
+});
